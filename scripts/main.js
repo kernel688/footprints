b26e8707808f86608ec67f49e8d6baa293dd70f9
kernel688 <jcmartinezcano@gmail.com>
2020-09-15 19:53:10 -0500

    New trx process flow and table appends

--- scripts/main.js
+++ scripts/main.js
@@ -1,37 +1,88 @@
+import {trxTimeStamp,trxDate,trxDateTime} from './sharedfunctions.js'
+
+
 let submitButton = document.getElementById('submitButton')
+submitButton.addEventListener('click',checkAddTrx)
 
-submitButton.addEventListener('click',getTrxValue)
 
-function getTrxValue() {
+function checkAddTrx() {
+    //*****************************//
+    //*** VARIABLE DECLARATIONS ***//
+    //*****************************//
     let getTrxType = Array.from(document.getElementsByName('trx-type'))
-    let checkedFields = 0
+    let getTrxAmount = Number(document.getElementById('trx-amount').value)
+    let getTrxDescription = String(document.getElementById('trx-description').value)
+    let getCurrentBalance = document.getElementById('currentBalance')
+    let getTableHeaders = document.getElementById('transactions-table-header')
     
+    let checkedFields = 0
+    let trxType = ''
+    let successfulCheck = 0
+    let successfulAdd = false
+
+    
+    //*************************************//
+    //*** CHECKS ALL TRANSACTION INPUTS ***//
+    //*************************************//
     for (let index = 0; index < getTrxType.length; index++) {
         if (getTrxType[index].checked) {
             trxType = getTrxType[index].value
-            console.log(trxType);
             checkedFields++
-            modifyBalance()
         }
     }
-    
-    if ( checkedFields === 0 ) {
+    if (checkedFields === 0) {
         window.alert('Please choose a transaction type')
-    }
-}
-
-
-function modifyBalance() {
-    let trxAmount = Number(document.getElementById('trx-amount').value)
-    let currentBalance = document.getElementById('currentBalance')
-    let currBal = Number(currentBalance.innerText)
-    
-    if (typeof currBal === 'number' && trxAmount !== 0 && trxType === 'Income') {
-        currentBalance.innerText = currBal + trxAmount
-    } else if (typeof currBal === 'number' && trxAmount !== 0 && trxType === 'Expense') {
-        currentBalance.innerText = currBal - trxAmount
     } else {
+        successfulCheck++
+    }
+
+    if (getTrxAmount === 0) {
         window.alert('Please enter a valid transaction amount')
+    } else {
+        successfulCheck++
+    }
+
+    if (getTrxDescription === '') {
+        window.alert('Please enter a transaction description')
+    } else {
+        successfulCheck++
+    }
+
+    
+    //***********************************************//
+    //*** ADDS TRANSACTION AFTER SUCCESSFUL CHECK ***//
+    //***********************************************//
+    if (successfulCheck === 3) {
+        let currBal = Number(getCurrentBalance.innerText)
+        if (typeof currBal === 'number' && trxType === 'Income') {
+            getCurrentBalance.innerText = currBal + getTrxAmount
+            successfulAdd = true
+        } else if (typeof currBal === 'number' && trxType === 'Expense') {
+            getCurrentBalance.innerText = currBal - getTrxAmount
+            successfulAdd = true
+        } else {
+            window.alert('k688: Undefined method for trxType')
+        }
+    }
+
+
+    //*****************************************************//
+    //*** SETS TIMESTAMP AND SHOWS TRANSACTION IN TABLE ***//
+    //*****************************************************//
+    if (successfulAdd) {
+        trxTimeStamp()
+        
+        getTableHeaders.insertAdjacentHTML('afterend',`
+        <tr class="transactions-table-data">
+            <td>${trxDate}</td>
+            <td>${trxType}</td>
+            <td>${getTrxAmount}</td>
+            <td>${getTrxDescription}</td>
+            <td><a id="trxID" class="trxDelete">Delete</a></td>
+            <td>Edit</td>
+        </tr>`)
     }
 
 }
+
+
