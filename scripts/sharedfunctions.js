0acc7568c9e4e0366944169d44a97179c066e271
kernel688 <jcmartinezcano@gmail.com>
2020-09-15 19:49:26 -0500

    Created sharedfunctions.js

---
+++ scripts/sharedfunctions.js
@@ -0,0 +1,16 @@
+export {trxTimeStamp,trxDate,trxDateTime}
+
+let trxDate
+let trxDateTime
+function trxTimeStamp() {
+    let trxFullDate = new Date()
+    let day = String(trxFullDate.getDate()).padStart(2,'0')
+    let month = String(trxFullDate.getMonth() + 1).padStart(2,'0')
+    let year = String(trxFullDate.getFullYear())
+    let hour = String(trxFullDate.getHours()).padStart(2,'0')
+    let minute = String(trxFullDate.getMinutes()).padStart(2,'0')
+    let second = String(trxFullDate.getSeconds()).padStart(2,'0')
+
+    trxDate = day + '/' + month + '/' + year
+    trxDateTime = year + month + day + hour + minute + second
+}
